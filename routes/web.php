b50c6878c4baae500091f3aba70a162e3f7f7324
hjcoder18 <hjmarshall18@gmail.com>
2017-09-30 11:18:41 -0600

    added all the page routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,39 @@
 Route::get('/', function () {
     return view('pages.welcome');
 });
+
+Route::get('home', function () {
+	return view('pages.welcome');
+});
+
+Route::get('events', function () {
+	return view('pages.events');
+});
+
+Route::get('gallery', function () {
+	return view('pages.gallery');
+});
+	
+Route::get('information', function () {
+	return view('pages.information');
+});
+	
+Route::get('litters', function () {
+	return view('pages.litter');
+});
+			
+Route::get('meet', function () {
+	return view('pages.meet');
+});
+			
+Route::get('place_order', function () {
+	return view('pages.place_order');
+});
+					
+Route::get('training', function () {
+	return view('pages.training');
+});
+						
+Route::get('gallery', function () {
+	return view('pages.gallery');
+});
