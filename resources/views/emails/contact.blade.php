5788ec7dbd2ac2014a4938444b9c6c432df521b2
hjcoder18 <hjmarshall18@gmail.com>
2017-10-30 14:00:03 -0600

    added email template

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,8 @@
+<h3>You Have a New Contact Via the Contact Form</h3>
+
+<div>
+	<p>{{ $name }} sent you an email saying...</p>
+	{{ $bodyMessage }}
+</div>
+<p>Phone number {{ $phone }}</p>
+<p>Sent via {{ $email }}</p>
